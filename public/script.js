8a75077479e9daa6cf1a4594ca2dee2e0be2ab04
Adam Shmeiss <34445682+adamico10@users.noreply.github.com>
2018-01-02 16:14:54 +0200

    shopping cart

--- public/script.js
+++ public/script.js
@@ -1,7 +1,15 @@
 // an array with all of our cart items
-var cart = [];
+var cart = {item: []};
 
 var updateCart = function () {
+  $('.cart-list').empty()
+  var source = $('#cart-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(cart);
+ 
+  // append our new html to the page
+  $('.cart-list').append(newHTML);
+  
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
@@ -9,6 +17,7 @@ var updateCart = function () {
 
 
 var addItem = function (item) {
+  cart.item.push(item)
   // TODO: Write this function. Remember this function has nothing to do with display. 
   // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
 }
@@ -18,11 +27,11 @@ var clearCart = function () {
 }
 
 $('.view-cart').on('click', function () {
-  // TODO: hide/show the shopping cart!
+  $('.shopping-cart').toggle();
 });
 
 $('.add-to-cart').on('click', function () {
-  // TODO: get the "item" object from the page
+  var item = $(this).closest('.item').data()
   addItem(item);
   updateCart();
 });
