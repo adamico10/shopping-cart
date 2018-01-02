254100630a305c2d51d1715fa348e0c242fb0b4f
Adam Shmeiss <34445682+adamico10@users.noreply.github.com>
2018-01-02 16:57:32 +0200

    update

--- public/script.js
+++ public/script.js
@@ -16,7 +16,7 @@ var updateCart = function () {
   $('.total').append(totalCartAmount)
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
-  // Remember to empty the "cart div" before you re-add all the item elements.
+// Remember to empty the "cart div" before you re-add all the item elements.
 }
 
 
